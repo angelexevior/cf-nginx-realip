a57ef173a0e19dd3171b3b09dc72257d3b9166c1
G4L1L3O <angelos@exevior.com>
2024-09-20 12:08:43 +0300

    Update cfips-nginx.sh
    Added comments and instructions in script

--- cfips-nginx.sh
+++ cfips-nginx.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 ###########################################
-# Developed by Angel Exevior
+# Developed by G4L1L3O
 # 
 #  << Instructions >>
-# - add a cronjob to run this command: 
+# - change the nginxpath below in configurations if your path is different. 
+# - add a cronjob to run this bash script @daily or @weekly /path_to_bash_script/cfips_nginx.sh
+# - make sure bash script can be executed by running this: chmod +x cfips-nginx.sh
+# - run the script once to create the include files 
+# - add this line in your nginx.conf file, within the http block
+#   "include cfips.txt;"
+# - restart nginx
+# - you should now have the real ip in your access logs
 #
 ###########################################
 
